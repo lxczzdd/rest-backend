670a671cd4d1cac53b54968b0a56141b13dac567
lxczzdd <stakoboshi@gmail.com>
2023-04-06 16:35:35 +0300

    fixed exception response

--- src/main/java/com/backend/app/users/exception/UserNotFoundAdvice.java
+++ src/main/java/com/backend/app/users/exception/UserNotFoundAdvice.java
@@ -1,14 +1,16 @@
 package com.backend.app.users.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class UserNotFoundAdvice {
+public class UserNotFoundAdvice extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(UserNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    String userNotFoundHandler(UserNotFoundException ex) {
-        return ex.getMessage();
+    ResponseEntity<String> userNotFoundHandler(UserNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 }
