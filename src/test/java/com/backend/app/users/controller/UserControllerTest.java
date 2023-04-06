8c66caa34f90971aab158d7c2e1f34f1974123d9
lxczzdd <stakoboshi@gmail.com>
2023-04-06 14:43:16 +0300

    users controller + service covered by tests

---
+++ src/test/java/com/backend/app/users/controller/UserControllerTest.java
@@ -0,0 +1,84 @@
+package com.backend.app.users.controller;
+
+import com.backend.app.users.entity.User;
+import com.backend.app.users.exception.UserNotFoundException;
+import com.backend.app.users.service.UserService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
+class UserControllerTest {
+    private static final long ID = 1L;
+
+    @Mock
+    private UserService service;
+
+    @InjectMocks
+    private UserController controller;
+
+    @Test
+    void getOneUserById_shouldCallService() {
+        final User user = Mockito.mock(User.class);
+        Mockito.when(service.getUserById(ID)).thenReturn(user);
+
+        final User actual = controller.getUserById(ID);
+
+        Assertions.assertNotNull(actual);
+        Assertions.assertEquals(user, actual);
+        Mockito.verify(service).getUserById(ID);
+    }
+
+    @Test
+    void getOneUserById_shouldThrowException_whenDoesNotExist() {
+        Mockito.when(service.getUserById(ID)).thenThrow(UserNotFoundException.class);
+
+        Assertions.assertThrows(UserNotFoundException.class, () -> controller.getUserById(ID));
+    }
+
+    @Test
+    void getAllUsers_shouldCallService() {
+        List<User> users = new ArrayList<>();
+        for(int i = 0; i < 3; ++i)
+            users.add(Mockito.mock(User.class));
+        Mockito.when(service.getAllUsers()).thenReturn(users);
+
+        final List<User> actualUsers = controller.getAllUsers();
+
+        Assertions.assertNotNull(actualUsers);
+        Assertions.assertEquals(users, actualUsers);
+        Mockito.verify(service).getAllUsers();
+    }
+
+    @Test
+    void createUser_shouldCallService() {
+        final User user = Mockito.mock(User.class);
+
+        controller.createUser(user);
+
+        Mockito.verify(service).createUser(user);
+    }
+
+    @Test
+    void deleteUserById_shouldCallService() {
+        controller.deleteUser(ID);
+
+        Mockito.verify(service).deleteUserById(ID);
+    }
+
+    @Test
+    void replaceUser_shouldCallService() {
+        final User user = Mockito.mock(User.class);
+
+        controller.replaceUser(user, ID);
+
+        Mockito.verify(service).replaceUser(user, ID);
+    }
+}
---
+++ src/test/java/com/backend/app/users/service/UserServiceImplTest.java
@@ -0,0 +1,92 @@
+package com.backend.app.users.service;
+
+import com.backend.app.users.entity.User;
+import com.backend.app.users.exception.UserNotFoundException;
+import com.backend.app.users.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceImplTest {
+
+    private static final long ID = 1L;
+
+    @Mock
+    private UserRepository repository;
+
+    @InjectMocks
+    private UserServiceImpl service;
+
+    @Test
+    void getOneUserById_shouldCallRepository() {
+        final User user = Mockito.mock(User.class);
+        Mockito.when(repository.findById(ID)).thenReturn(Optional.of(user));
+
+        final User actual = service.getUserById(ID);
+
+        Assertions.assertNotNull(actual);
+        Assertions.assertEquals(user, actual);
+        Mockito.verify(repository).findById(ID);
+    }
+
+    @Test
+    void getOneUserById_shouldThrowException_whenDoesNotExist() {
+        Assertions.assertThrows(UserNotFoundException.class, () -> service.getUserById(ID));
+    }
+
+    @Test
+    void getAllUsers_shouldCallRepository() {
+        List<User> users = new ArrayList<>();
+        for(int i = 0; i < 3; ++i)
+            users.add(Mockito.mock(User.class));
+        Mockito.when(repository.findAll()).thenReturn(users);
+
+        final List<User> actualUsers = service.getAllUsers();
+
+        Assertions.assertNotNull(actualUsers);
+        Assertions.assertEquals(users, actualUsers);
+        Mockito.verify(repository).findAll();
+    }
+
+    @Test
+    void createUser_shouldCallRepository() {
+        final User user = Mockito.mock(User.class);
+
+        service.createUser(user);
+
+        Mockito.verify(repository).save(user);
+    }
+
+    @Test
+    void deleteUserById_shouldCallRepository() {
+        final User user = Mockito.mock(User.class);
+        Mockito.when(repository.findById(ID)).thenReturn(Optional.of(user));
+
+        service.deleteUserById(ID);
+
+        Mockito.verify(repository).delete(user);
+    }
+
+    @Test
+    void replaceUser_shouldCallRepository() {
+        final User updatedUser = new User("newName", "newemail@example.com", "252525");
+        final User user = Mockito.mock(User.class);
+        Mockito.when(repository.findById(ID)).thenReturn(Optional.of(user));
+        Mockito.when(repository.save(user)).thenReturn(updatedUser);
+
+        final User actual = service.replaceUser(user, ID);
+
+        Assertions.assertNotNull(actual);
+        Assertions.assertNotEquals(user, actual);
+        Mockito.verify(repository).save(user);
+    }
+}
